b4e8dfb1d6bc9beddd630f639c706f1ddfb15254
ChiaMineJP <admin@chiamine.jp>
2021-05-14 23:53:53 +0900

    Added types for wallet rpc api

--- src/api/chia/types/_python_types_.ts
+++ src/api/chia/types/_python_types_.ts
@@ -1,7 +1,7 @@
 export type G1Element = unknown; 
 export type G2Element = unknown; // see https://pypi.org/project/blspy/
 
-export type bytes = unknown;
+export type bytes = string;
 
 export type int = number;
 export type int8 = number;
