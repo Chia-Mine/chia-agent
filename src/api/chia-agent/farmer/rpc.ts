c7591002144b8cd2cee916d6aff1542d6dbc6109
ChiaMineJP <admin@chiamine.jp>
2021-05-15 03:53:18 +0900

    Implementing rpc/ws api

--- src/api/chia-agent/farmer/rpc.ts
+++ src/api/chia-agent/farmer/rpc.ts
@@ -1,10 +1,10 @@
 import {ProofOfSpace} from "../../chia/types/blockchain_format/proof_of_space";
 import {bool, str, uint64, uint8} from "../../chia/types/_python_types_";
 import {bytes32} from "../../chia/types/blockchain_format/sized_bytes";
+import {IAgent} from "../../../agent.type";
 
 export const serviceName = "chia_farmer";
 
-export const get_signage_point = "get_signage_point";
 export type TGetSignagePointRequest = {
   sp_hash: str;
 };
@@ -19,19 +19,24 @@ export type TGetSignagePointResponse = {
   },
   proofs: [string, ProofOfSpace],
 };
+export async function get_signage_point(agent: IAgent<TGetSignagePointResponse>, data: TGetSignagePointRequest){
+  const command = "get_signage_point";
+  return agent.sendMessage(serviceName, command, data);
+}
 
 
-
-export const get_signage_points = "get_signage_points";
 export type TGetSignagePointsRequest = {
 };
 export type TGetSignagePointsResponse = {
   signage_points: TGetSignagePointResponse[];
 };
+export async function get_signage_points(agent: IAgent<TGetSignagePointsResponse>, data: TGetSignagePointsRequest){
+  const command = "get_signage_points";
+  return agent.sendMessage(serviceName, command, data);
+}
 
 
 
-export const get_reward_targets = "get_reward_targets";
 export type TGetRewardTargetRequest = {
   search_for_private_key: bool;
 };
@@ -44,13 +49,20 @@ export type TGetRewardTargetResponse = {
   farmer_target: str;
   pool_target: str;
 };
+export async function get_reward_targets(agent: IAgent<TGetRewardTargetResponse>, data: TGetRewardTargetRequest){
+  const command = "get_reward_targets";
+  return agent.sendMessage(serviceName, command, data);
+}
 
 
 
-export const set_reward_targets = "set_reward_targets";
 export type TSetRewardTargetRequest = {
   farmer_target?: str;
   pool_target?: str;
 };
 export type TSetRewardTargetResponse = {
 };
+export async function set_reward_targets(agent: IAgent<TSetRewardTargetResponse>, data: TSetRewardTargetRequest){
+  const command = "set_reward_targets";
+  return agent.sendMessage(serviceName, command, data);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./logger";
 export * from "./daemon/index";
 export * from "./config/index";
+export {TMessage} from "./agent.type";
