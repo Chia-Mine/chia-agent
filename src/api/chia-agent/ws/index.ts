0540e406fc9b21cf07953333f5a890c29efeace7
ChiaMineJP <admin@chiamine.jp>
2021-05-17 02:08:43 +0900

    Checkpoint. (some of the files are wip so there might be errors)

--- src/api/chia-agent/ws/index.ts
+++ src/api/chia-agent/ws/index.ts
@@ -6,21 +6,59 @@ import type {
   TNewFarmingInfoBroadCast,
   TNewSignagePointBroadCast,
 } from "./farmer";
+export * from "./farmer";
+
 import type {
   chia_full_node_service,
   get_blockchain_state_command,
   TGetBlockchainStateBroadCast,
 } from "./full_node";
+export * from "./full_node";
+
 import type {
   chia_harvester_service,
   get_plots_command,
   TGetPlotsBroadCast,
 } from "./harvester";
+export * from "./harvester";
+
 import type {
   chia_wallet_service,
   state_changed_command,
   TStateChangedBroadCast,
 } from "./wallet";
+export * from "./wallet";
+
+import type {
+  chia_plots_create_service,
+  state_changed_command as state_change_command_of_plots,
+  TStateChangedBroadCast as TStateChangedOfPlotsBroadCast,
+} from "./chia_plots_create";
+export * from "./wallet";
+
+import type {
+  daemon_service,
+  exit_command,
+  get_status_command,
+  is_running_command,
+  ping_command,
+  register_service_command,
+  start_plotting_command,
+  start_service_command,
+  stop_plotting_command,
+  stop_service_command,
+  TExitResponse,
+  TGetStatusResponse,
+  TIsRunningResponse,
+  TPingResponse,
+  TRegisterServiceResponse,
+  TStartPlottingResponse,
+  TStartServiceResponse,
+  TStopPlottingResponse,
+  TStopServiceResponse,
+} from "./daemon";
+export * from "./daemon";
+
 
 export type WsFarmerMessage =
   GetMessageType<chia_farmer_service, new_farming_info_command, TNewFarmingInfoBroadCast>
@@ -28,8 +66,7 @@ export type WsFarmerMessage =
   ;
 
 export type WsFullNodeMessage =
-  GetMessageType<chia_full_node_service, get_blockchain_state_command, TGetBlockchainStateBroadCast>
-;
+  GetMessageType<chia_full_node_service, get_blockchain_state_command, TGetBlockchainStateBroadCast>;
 
 export type WsHarvesterMessage =
   GetMessageType<chia_harvester_service, get_plots_command, TGetPlotsBroadCast>;
@@ -37,4 +74,21 @@ export type WsHarvesterMessage =
 export type WsWalletMessage =
   GetMessageType<chia_wallet_service, state_changed_command, TStateChangedBroadCast>;
 
-export type WsMessage = WsFarmerMessage | WsFullNodeMessage | WsHarvesterMessage | WsWalletMessage;
+export type WsPlotsMessage =
+  GetMessageType<chia_plots_create_service, state_change_command_of_plots, TStateChangedOfPlotsBroadCast>;
+
+export type WsDaemonMessage =
+  GetMessageType<daemon_service, exit_command, TExitResponse>
+  | GetMessageType<daemon_service, get_status_command, TGetStatusResponse>
+  | GetMessageType<daemon_service, is_running_command, TIsRunningResponse>
+  | GetMessageType<daemon_service, ping_command, TPingResponse>
+  | GetMessageType<daemon_service, register_service_command, TRegisterServiceResponse>
+  | GetMessageType<daemon_service, start_plotting_command, TStartPlottingResponse>
+  | GetMessageType<daemon_service, start_service_command, TStartServiceResponse>
+  | GetMessageType<daemon_service, stop_plotting_command, TStopPlottingResponse>
+  | GetMessageType<daemon_service, stop_service_command, TStopServiceResponse>;
+
+
+
+export type WsMessage = WsFarmerMessage | WsFullNodeMessage | WsHarvesterMessage | WsWalletMessage | WsPlotsMessage
+  | WsDaemonMessage;
