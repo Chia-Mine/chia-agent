061c9b35a2c15b542a2c4f0c62c2b69c1ceaf93f
ChiaMineJP <admin@chiamine.jp>
2021-05-19 01:59:42 +0900

    Added ws api document for chia_plots_create

--- src/api/ws/chia_plots_create/index.ts
+++ src/api/ws/chia_plots_create/index.ts
@@ -6,16 +6,16 @@ import {WsMessage} from "../index";
 export const chia_plots_create_service = "chia plots create";
 export type chia_plots_create_service = typeof chia_plots_create_service;
 
-export const state_changed_command = "state_changed";
-export type state_changed_command = typeof state_changed_command;
-export type TStateChangedBroadCast = {
+export const state_changed_command_of_plots = "state_changed";
+export type state_changed_command_of_plots = typeof state_changed_command_of_plots;
+export type TStateChangedBroadCastOfPlots = {
   state: "log_changed"|"state_changed";
   queue: TPlotQueue[];
 };
-export async function on_state_changed(daemon: TDaemon, callback: (e: GetMessageType<chia_plots_create_service, state_changed_command, TStateChangedBroadCast>) => unknown){
+export async function on_state_changed_of_plots(daemon: TDaemon, callback: (e: GetMessageType<chia_plots_create_service, state_changed_command_of_plots, TStateChangedBroadCastOfPlots>) => unknown){
   await daemon.subscribe(chia_plots_create_service);
   const messageListener = (e: WsMessage) => {
-    if(e.origin === chia_plots_create_service && e.command === state_changed_command){
+    if(e.origin === chia_plots_create_service && e.command === state_changed_command_of_plots){
       callback(e);
     }
   };
