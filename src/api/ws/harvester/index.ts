346052c95a299ca460bffd068d0c6a63125478c4
ChiaMineJP <admin@chiamine.jp>
2021-05-19 00:59:23 +0900

    Added websocket listner APIs

--- src/api/ws/harvester/index.ts
+++ src/api/ws/harvester/index.ts
@@ -1,4 +1,7 @@
 import {Plot} from "../../chia/harvester/harvester";
+import {TDaemon} from "../../../daemon/index";
+import {GetMessageType, wallet_ui_service} from "../../types";
+import {WsMessage} from "../index";
 
 export const chia_harvester_service = "chia_harvester";
 export type chia_harvester_service = typeof chia_harvester_service;
@@ -10,3 +13,12 @@ export type TGetPlotsBroadCast = {
   failed_to_open_filenames: string[];
   not_found_filenames: string[];
 };
+export async function on_get_plots(daemon: TDaemon, callback: (e: GetMessageType<chia_harvester_service, get_plots_command, TGetPlotsBroadCast>) => unknown){
+  await daemon.subscribe(wallet_ui_service);
+  const messageListener = (e: WsMessage) => {
+    if(e.origin === chia_harvester_service && e.command === get_plots_command){
+      callback(e);
+    }
+  };
+  return daemon.addMessageListener(chia_harvester_service, messageListener);
+}
