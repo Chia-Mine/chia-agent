1306e99d4735801806b652e4f176047e9672989e
ChiaMineJP <admin@chiamine.jp>
2021-05-19 13:00:06 +0900

    Fixed sample code

--- example/send_email_when_proof_is_found/main.js
+++ example/send_email_when_proof_is_found/main.js
@@ -13,24 +13,22 @@ async function main(){
   await daemon.connect();
   
   const unsubscribe = await on_new_farming_info(daemon, async (e) => {
-    if(e.command === "new_farming_info"){
-      const {farming_info} = e.data;
-      const {challenge_hash, passed_filter, proofs, total_plots, timestamp} = farming_info;
-      const date = new Date(timestamp*1000);
-    
-      console.log(`${challenge_hash.substr(0, 32)}... ${passed_filter}/${total_plots} ${proofs} ${date.toLocaleTimeString()}`);
-    
-      if(proofs > 0){
-        // Send email with Gmail
-        const subject = "[Chia] A proof has been found!";
-        const mailBody = "Go check your wallet to see your earning";
-        const from = "XXX <xxx@xxx.xxx>"; // Edit here
-        const to = "YYY <yyy@yyy.yyy>"; // Edit here
-        await sendMail(subject, mailBody, from, to).catch(error => {
-          console.error("Failed to send email");
-          console.error(JSON.stringify(error));
-        });
-      }
+    const {farming_info} = e.data;
+    const {challenge_hash, passed_filter, proofs, total_plots, timestamp} = farming_info;
+    const date = new Date(timestamp*1000);
+  
+    console.log(`${challenge_hash.substr(0, 32)}... ${passed_filter}/${total_plots} ${proofs} ${date.toLocaleTimeString()}`);
+  
+    if(proofs > 0){
+      // Send email with Gmail
+      const subject = "[Chia] A proof has been found!";
+      const mailBody = "Go check your wallet to see your earning";
+      const from = "XXX <xxx@xxx.xxx>"; // Edit here
+      const to = "YYY <yyy@yyy.yyy>"; // Edit here
+      await sendMail(subject, mailBody, from, to).catch(error => {
+        console.error("Failed to send email");
+        console.error(JSON.stringify(error));
+      });
     }
   });
 }
