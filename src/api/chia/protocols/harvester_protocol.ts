28c40b35d114b1d8bc3535a573206fb32650c02a
ChiaMineJP <admin@chiamine.jp>
2021-05-27 00:17:44 +0900

    Implemented new apis for farmer

---
+++ src/api/chia/protocols/harvester_protocol.ts
@@ -0,0 +1,19 @@
+import {G1Element, Optional, str, uint64, uint8} from "../types/_python_types_";
+import {bytes32} from "../types/blockchain_format/sized_bytes";
+
+export type Plot = {
+  filename: str;
+  size: uint8;
+  plot_id: bytes32;
+  pool_public_key: Optional<G1Element>;
+  pool_contract_puzzle_hash: Optional<bytes32>;
+  plot_public_key: G1Element;
+  file_size: uint64;
+  time_modified: uint64;
+};
+
+export type RespondPlots = {
+  plots: Plot[];
+  failed_to_open_filenames: str[];
+  no_key_filenames: str[];
+};
--- src/api/rpc/farmer/index.ts
+++ src/api/rpc/farmer/index.ts
@@ -1,7 +1,10 @@
 import {ProofOfSpace} from "../../chia/types/blockchain_format/proof_of_space";
-import {bool, str, uint64, uint8} from "../../chia/types/_python_types_";
+import {bool, bytes, int, Optional, str, uint16, uint64, uint8} from "../../chia/types/_python_types_";
 import {bytes32} from "../../chia/types/blockchain_format/sized_bytes";
 import {TRPCAgent} from "../../../rpc/index";
+import {TPoolInfoResponse} from "../pool/index";
+import {PoolConfig} from "../../chia/farmer/pool_config";
+import {RespondPlots} from "../../chia/protocols/harvester_protocol";
 
 export const chia_farmer_service = "chia_farmer";
 export type chia_farmer_service = typeof chia_farmer_service;
@@ -70,4 +73,62 @@ export type TSetRewardTargetResponse = {
 };
 export async function set_reward_targets(agent: TRPCAgent, params: TSetRewardTargetRequest){
   return agent.sendMessage<TSetRewardTargetResponse>(chia_farmer_service, set_reward_targets_command, params);
+}
+
+
+
+export type PoolState = {
+  points_found_since_start: int;
+  points_found_24h: unknown[];
+  points_acknowledged_since_start: int;
+  points_acknowledged_24h: unknown[];
+  current_points_balance: int;
+  current_difficulty: int;
+  pool_errors_24h: unknown[];
+  pool_info: TPoolInfoResponse;
+  pool_config: PoolConfig;
+  p2_singleton_puzzle_hash: str;
+};
+
+export const get_pool_state_command = "get_pool_state";
+export type get_pool_state_command = typeof get_pool_state_command;
+export type TGetPoolStateRequest = {
+};
+export type TGetPoolStateResponse = {
+  pool_state: PoolState[];
+};
+export async function get_pool_state(agent: TRPCAgent){
+  return agent.sendMessage<TSetRewardTargetResponse>(chia_farmer_service, get_pool_state_command);
+}
+
+
+
+export const set_pool_payout_instructions_command = "set_pool_payout_instructions";
+export type set_pool_payout_instructions_command = typeof set_pool_payout_instructions_command;
+export type TSetPoolPayoutInstructionsRequest = {
+  singleton_genesis: str;
+  pool_payout_instructions: str;
+};
+export type TSetPoolPayoutInstructionsResponse = {
+};
+export async function set_pool_payout_instructions(agent: TRPCAgent, params: TSetPoolPayoutInstructionsRequest){
+  return agent.sendMessage<TSetPoolPayoutInstructionsResponse>(chia_farmer_service, set_pool_payout_instructions_command, params);
+}
+
+
+
+export type RequestPlotsResponse = {
+  type: 68;
+  id: Optional<uint16>;
+  data: RespondPlots;
+};
+export const get_plots_command = "get_plots";
+export type get_plots_command = typeof get_plots_command;
+export type TGetPlotsRequest = {
+};
+export type TGetPlotsResponse = {
+  [peer_host_port: string]: RequestPlotsResponse;
+};
+export async function get_plots(agent: TRPCAgent){
+  return agent.sendMessage<TGetPlotsResponse>(chia_farmer_service, get_plots_command);
 }
