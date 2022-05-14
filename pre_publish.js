ab410668299ab9f3c778e323b282b43d594f9505
ChiaMineJP <admin@chiamine.jp>
2022-05-14 18:25:53 +0900

    Replaced `rmdirSync` with `rmSync`

--- pre_publish.js
+++ pre_publish.js
@@ -7,7 +7,7 @@ const devMode = process.argv.length !== 3 || process.argv[2] !== "--prod";
 const distDirName = devMode ? "build" : "dist";
 const distDir = path.join(__dirname, distDirName);
 if(fs.existsSync(distDir)){
-  fs.rmdirSync(distDir, {recursive: true});
+  fs.rmSync(distDir, {recursive: true});
 }
 fs.mkdirSync(distDir);
 
