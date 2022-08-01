4a173bff6593af624fa13dea345944fb86f3b9ff
ChiaMineJP <admin@chiamine.jp>
2022-08-01 20:27:22 +0900

    Updated src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ export * from "./logger";
 export * from "./daemon/index";
 export * from "./rpc/index";
 export * from "./config/index";
-export {TDriverDict} from "./api/chia/wallet/puzzle_drivers";
-export {TDriverDictValue} from "./api/chia/wallet/puzzle_drivers";
